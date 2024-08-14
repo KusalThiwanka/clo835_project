7bf2450f2bf036f3f9b6a72586766fbc719e1541
KusalThiwanka <mhkusalthiwanka@gmail.com>
2024-08-14 01:47:07 -0400

    Initilized Project

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+export const PORT = 5555;
+
+// export const mongoDBURL =
+//   'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+export const mongoDBURL =
+  'mongodb+srv://kusal:kusal123@fullstackapp.au0pq.mongodb.net/books-collection?retryWrites=true&w=majority&appName=FullStackApp';
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
